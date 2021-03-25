09800477300a72515e15f0a87585fbc9555efc13
tmknom <tmk.nom@gmail.com>
2021-03-25 09:46:48 +0900

    initserver関数がアドレスを再利用できるよう改善

--- chap16/server1.c
+++ chap16/server1.c
@@ -81,9 +81,12 @@ int main(int argc, char *argv[]) {
 int initserver(int type, const struct sockaddr *addr, socklen_t alen, int qlen) {
     int fd;
     int err = 0;
+    int reuse = 1;
 
     if ((fd = socket(addr->sa_family, type, 0)) < 0)
         return (-1);
+    if (setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(int)) < 0)
+        goto errout;
     if (bind(fd, addr, alen) < 0)
         goto errout;
     if (type == SOCK_STREAM || type == SOCK_SEQPACKET) {
